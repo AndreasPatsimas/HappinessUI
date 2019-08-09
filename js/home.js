b207e33f3f31131d27efa64ca0f731b53418b466
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-08-09 17:03:11 +0300

    upadates

--- js/home.js
+++ js/home.js
@@ -1,42 +1,52 @@
 const liHome       = document.querySelector("#home"),
-      aHome        = liHome .firstChild,
-      divHomeInfo  = document.querySelector("#homeInfo"),
-      general      = document.querySelector("#general"),
-      january      = document.querySelector("#January"),
-      february     = document.querySelector("#February"),
-      march        = document.querySelector("#March"),  
-      april        = document.querySelector("#April"),  
-      may          = document.querySelector("#May"),
-      june         = document.querySelector("#June"),
-      july         = document.querySelector("#July"),
-      august       = document.querySelector("#August"),
-      september    = document.querySelector("#September"),
-      october      = document.querySelector("#October"),
-      november     = document.querySelector("#November"),
-      december     = document.querySelector("#December");
+aHome        = liHome .firstChild,
+divHomeInfo  = document.querySelector("#homeInfo"),
+general      = document.querySelector("#general"),
+january      = document.querySelector("#January"),
+february     = document.querySelector("#February"),
+march        = document.querySelector("#March"),  
+april        = document.querySelector("#April"),  
+may          = document.querySelector("#May"),
+june         = document.querySelector("#June"),
+july         = document.querySelector("#July"),
+august       = document.querySelector("#August"),
+september    = document.querySelector("#September"),
+october      = document.querySelector("#October"),
+november     = document.querySelector("#November"),
+december     = document.querySelector("#December");
 
-      
+
 aHome.addEventListener("click", function(){
 
-    changeTab(liHome, liTasks, liSettings, divHomeInfo, divTasksInfo, divSettingsInfo);
-    
-    document.getElementById("myActivitiesTabs").style.display = "block";
+changeTab(liHome, liTasks, liSettings, divHomeInfo, divTasksInfo, divSettingsInfo);
+
+document.getElementById("myActivitiesTabs").style.display = "block";
 });
 
+const httpHome = new httpRequest();
+
+httpHome.get("happyUsers.json", function(error, user){
+
+if(error){
+console.log(error);
+}
+else{
+
+user = JSON.parse(user);
 
 function init(...tabs){
 
-    tabs.forEach(function(tabActivated){
-        if(tabActivated != null){
-            tabActivated.addEventListener("click",function(){
-                
-                tab = getActiveTab();
-                
-                changeMonthsTab(tabActivated,tab);
-            
-            });
-        }
-    });
+tabs.forEach(function(tabActivated){
+  if(tabActivated != null){
+      tabActivated.addEventListener("click",function(){
+          
+          tab = getActiveTab();
+          
+          changeMonthsTab(tabActivated,tab);
+      
+      });
+  }
+});
 }
 
 init(general, january, february, march, april, may, june, july, august, september, october, november, december);
@@ -45,58 +55,77 @@ init(general, january, february, march, april, may, june, july, august, septembe
 var tab;
 
 function getActiveTab(){
-    
-    var tabListItems = document.getElementById('myActivitiesTabs').childNodes;
-    
-    tabListItems.forEach(function(tabListItem){
-        
-        if(tabListItem.nodeName == "LI" && tabListItem.className == "active"){
-           
-           tab = tabListItem; 
-                  
-        }
-    });
-    
-    return tab;
+
+var tabListItems = document.getElementById('myActivitiesTabs').childNodes;
+
+tabListItems.forEach(function(tabListItem){
+  
+  if(tabListItem.nodeName == "LI" && tabListItem.className == "active"){
+     
+     tab = tabListItem; 
+            
+  }
+});
+
+return tab;
 }
 
 
 function changeMonthsTab(activeElement, inactiveElement) {
 
-    if(activeElement != null){
-                    
-            activeElement.setAttribute("class","active");
+if(activeElement != null){
+              
+      activeElement.setAttribute("class","active");
 
-            inactiveElement.removeAttribute("class");
-            
-            fillhomeTable(activeElement);
-    }  
+      inactiveElement.removeAttribute("class");
+      
+      fillhomeTable(activeElement);
+}  
 }
 
 function fillhomeTable(activeElement){
-    const homeTable = document.getElementById("items");
-            
-            var content = "";
+const homeTable = document.getElementById("items");
+      
+      var content = "";
 
-            for(let i = 1; i < 4; i++){
-                    content +=      `<tr>
-                                            <td>${i}</td>
-                                            <td>${activeElement.id}</td>
-                                            <td>${activeElement.id}</td>
-                                            <td>${activeElement.id}</td>
-                                    </tr>`;
-            }
+      for(let i = 1; i < 4; i++){
+              content +=      `<tr>
+                                      <td>${i}</td>
+                                      <td>${activeElement.id}</td>
+                                      <td>${activeElement.id}</td>
+                                      <td>${activeElement.id}</td>
+                              </tr>`;
+      }
 
-            homeTable.innerHTML = content;
+      homeTable.innerHTML = content;
 }
 
 fillhomeTable(general);
 
+httpHome.get(`previousYearProfile.json`, 
+  function(error,previousYearProfiles){
+      if(error){
+          console.log(error);
+      }
+        else{
+            previousYearProfiles = JSON.parse(previousYearProfiles);
+            console.log(previousYearProfiles);
+            let today = new Date();
+
+            let avgRatingPreviousYear = Math.round(71.25 * 100) / 100;
+            
+            document.getElementById("avgPrev").innerHTML = `<a href = "#" style = "color:grey" id = "analysisPreviousYear"><span class="pull-left"><strong id="prevYear">
+                                                          General ${today.getFullYear()-1}</strong></span> 
+                                                          ${avgRatingPreviousYear}% </a>`;
+
+            
+                              
+      }
+});
+
+
+}
+});
 
 
 
-
-
-
-
-    
--- js/profile.js
+++ js/profile.js
@@ -46,9 +46,6 @@ let lastLogin = getBeautifulDate(d);
 
 const lastLog = document.getElementById("lastLogin").innerHTML = '<span class="pull-left"><strong>Last seen</strong></span> '+ lastLogin;
 
-document.getElementById("prevYear").textContent = `General ${d.getFullYear() - 1}`;
-document.getElementById("thisYear").textContent = `General ${d.getFullYear()}`;
-
 document.getElementById("imageProfile").addEventListener("click", function(){
 	
 	const imageForm = document.getElementById("new_picture");
